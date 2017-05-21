88fcaabe7aadeba41da845833fca0fe7c8cf9b21
miha2158 <miha.vanya@gmail.com>
2017-05-22 01:25:43 +0500

    'boom'

---
+++ New_Collections/ArrayList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Collections
+{
+    public class ArrayList : ICloneable
+    {
+        public int Capacity
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            }
+        }
+
+        public void Clear()
+        {
+
+        }
+        public object Clone()
+        {
+            return null;
+        }
+
+        public int IndexOf(object v)
+        {
+            return 0;
+        }
+        public int LastIndexOf(object v)
+        {
+            return 0;
+        }
+        
+        public void Add(object v)
+        {
+            
+        }
+        public void Insert(int index, object v)
+        {
+            
+        }
+
+        public void Remove(object v)
+        {
+            
+        }
+        public void RemoveAt(object v)
+        {
+
+        }
+
+        public void Reverse()
+        {
+            
+        }
+
+        public void Sort()
+        {
+            
+        }
+        public void Sort(IComparer comparer)
+        {
+
+        }
+
+        public bool BinarySearch(object v)
+        {
+            return false;
+        }
+        public bool BinarySearch(object v, IComparer comparer)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ New_Collections/MyDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Collections
+{
+    public class MyDictionary<T>
+    {
+        
+    }
+}
