6085cb55473e8a206796daf91ef6b9e896046346
miha2158 <miha.vanya@gmail.com>
2017-05-31 12:19:23 +0500

    totally done

--- Lab12/Program.cs
+++ Lab12/Program.cs
@@ -2,6 +2,12 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using Countries_Lab11;
+using New_Collections;
+
+using static System.Console;
 
 namespace Lab12
 {
@@ -9,8 +15,144 @@ namespace Lab12
     {
         static void Main(string[] args)
         {
-            
+            #region ArrayList
 
+            WriteLine("ArrayList");
+            var Al = new ArrayList(5);
+            Al.Add("123");
+            Al.Add(122);
+            Al.Add('c');
+            Al.Add(new List<int>(0));
+            
+            foreach (var v in Al)
+                WriteLine("{0}",v);
+            WriteLine();
+
+            Al[2] = 1;
+            Al[1] = new DateTime(2017,5,25,13,49,54);
+            Al[3] = new int[0];
+
+            foreach (var v in Al)
+                WriteLine("{0}", v);
+            WriteLine();
+
+            try
+            {
+                WriteLine("индекс \'new int[0]\' - {0}, Обычный поиск\n", Al.IndexOf(new int[0]));
+            }
+            catch (Exception)
+            {
+                WriteLine("No");
+            }
+            WriteLine();
+
+            try
+            {
+                Al.Sort();
+            }
+            catch (Exception)
+            {
+                WriteLine("Not sorted");
+            }
+            WriteLine();
+
+            foreach (var v in Al)
+                WriteLine("{0}", v);
+            WriteLine();
+
+            try
+            {
+                WriteLine("индекс \'new int[0]\' - {0}, Бинарный поиск\n", Al.BinarySearch(new int[0]));
+            }
+            catch (Exception)
+            {
+                WriteLine("Unable to find");
+            }
+            WriteLine();
+
+            Al.Clear();
+            foreach (var v in Al)
+                WriteLine("{0}", v);
+            WriteLine();
+
+            #endregion
+
+
+            #region Stack
+
+            WriteLine();
+            WriteLine("Stack");
+            WriteLine();
+
+            var st = new Stack<string>();
+            
+            st.Push("-0");
+            foreach (string s in st)
+                WriteLine(s);
+            WriteLine();
+
+            st.Push(st.Peek());
+            st.Push("12345");
+            st.Push(st.Count.ToString());
+            foreach (string s in st)
+                WriteLine(s);
+            WriteLine();
+
+            st.Push(st.Pop());
+            foreach (string s in st)
+                WriteLine(s);
+            WriteLine();
+
+            st.Clear();
+            foreach (string s in st)
+                WriteLine(s);
+            WriteLine();
+
+            #endregion Stack
+
+
+            #region MyDictionary
+
+            WriteLine();
+            WriteLine("MyDictionary");
+            WriteLine();
+
+            var md = new MyDictionary<Country>(3);
+            
+            md.Add(new Republic("1",900,"C1","R1",new []{"P1"}));
+
+            var mdCopy = (MyDictionary<Country>)md.Clone();
+
+            foreach (Country c in mdCopy.Values)
+                WriteLine(c);
+            WriteLine();
+
+            var mon = new Monarchy("2", 5, "55", "R1");
+            md.Add(mon);
+            mdCopy.Add(new Kingdom("2",100,"C2","K1"));
+
+            foreach (Country c in mdCopy.Values)
+                WriteLine(c);
+            WriteLine();
+
+            foreach (Country c in md.Values)
+                WriteLine(c);
+            WriteLine();
+
+            md.AddRange(mdCopy);
+
+            foreach (Country c in mdCopy.Values)
+                WriteLine(c);
+            WriteLine();
+
+            mdCopy.Clear();
+
+            WriteLine("md.comtains('{0}') == '{1}'",mon,md.ContainsValue(mon));
+            md.Remove(mon);
+            
+            #endregion
+
+            ReadKey(true);
         }
     }
 }
--- New_Collections/MyDictionary.cs
+++ New_Collections/MyDictionary.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 
 namespace New_Collections
 {
-    public class MyDictionary<T> : ICloneable, IEnumerable<T> where T : ICloneable, IEnumerator<T>
+    public class MyDictionary<T> : ICloneable, IEnumerable<T> where T : ICloneable
     {
         protected T[] _items = new T[50];
+        public T[] Values
+        {
+            get
+            {
+                var t = new List<T>(0);
+                foreach (T item in _items)
+                    if (item.Equals(default(T)) || item.Equals(null))
+                        t.Add(item);
+
+                return t.ToArray();
+            }
+        }
         protected float FillRatio = 0.72f;
 
         protected int Capacity
@@ -17,8 +29,8 @@ namespace New_Collections
             get { return _items.Length; }
             set
             {
-                if (Capacity < value)
-                    current = value;
+                if (Count < value)
+                    itemCount = value;
 
                 var temp = new T[value];
                 try
@@ -35,11 +47,11 @@ namespace New_Collections
                 _items = temp;
             }
         }
-        protected int current;
+        protected int itemCount;
 
         public int Count
         {
-            get { return current; }
+            get { return itemCount; }
         }
         public object this[int i]
         {
@@ -70,13 +82,15 @@ namespace New_Collections
         public MyDictionary(IEnumerable<T> obj)
         {
             Capacity = obj.Count();
-            _items = obj.ToArray();
+            foreach (T b in obj)
+                Add(b);
         }
         public MyDictionary(IEnumerable<T> obj, float FillRatio)
         {
             this.FillRatio = FillRatio;
             Capacity = obj.Count();
-            _items = obj.ToArray();
+            foreach (T b in obj)
+                Add(b);
         }
 
         #endregion
@@ -84,34 +98,44 @@ namespace New_Collections
         public void Clear()
         {
             Capacity = 0;
-            current = 0;
+            itemCount = 0;
         }
         public object Clone()
         {
-            return new MyDictionary<T>(_items);
+            return new MyDictionary<T>((T[])_items.Clone());
         }
         
-
         public void Add(T o)
         {
-            if (Math.Abs(Count - Capacity*FillRatio) < 0.01f)
+            while (Math.Abs(Count - Capacity*FillRatio) < 0.03f ||
+               Exists(Array.IndexOf(_items, o)))
                 Capacity *= 2;
 
-            _items[current++] = o;
+            _items[o.GetHashCode()%Capacity] = o;
+            itemCount++;
+        }
+        public void AddRange(IEnumerable<T> o)
+        {
+            foreach (T item in o)
+                Add(item);
         }
         public void Remove(T o)
         {
             int index = Array.IndexOf(_items, o);
 
             if (index >= 0 && index <= Capacity)
-            {
-                for (int i = index; i < Count; i++)
-                    _items[i] = _items[i + 1];
-
-                current--;
-            }
+                _items[index] = default(T);
             else
                 throw new IndexOutOfRangeException();
+        }
+
+        public bool ContainsValue(T o)
+        {
+            return _items.Contains(o);
+        }
+        protected bool Exists(int index)
+        {
+            return _items[index].Equals(default(T)) || _items[index].Equals(null);
         }
 
         public IEnumerator<T> GetEnumerator()
